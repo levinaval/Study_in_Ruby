51da29f9fac73448a0a012e67abc83e78982b477
levi <levisantosluna@gmail.com>
2022-05-17 09:49:22 -0300

    reverse

--- Try_Ruby/repetiçoes.rb
+++ Try_Ruby/repetiçoes.rb
@@ -1,3 +1,11 @@
-3.times do
-  print "Welcome, "
+#3.times do
+#  print "Welcome, "
+#end
+
+def repeticao
+
+ puts "Welcome"
+
 end
+
+repeticao
---
+++ Try_Ruby/reverse.rb
@@ -0,0 +1,9 @@
+def fist_name
+
+  puts "Digite seu nome completo."
+   
+  fist_name = gets.chomp
+  
+end
+
+puts fist_name.reverse
