ceed1a44f4e957dbf7bb0ef7497f82ab02d9c198
levi <levisantosluna@gmail.com>
2022-06-02 20:48:32 -0300

    test_instance_variables_can_be_set_by_assigning_to_them

--- Koans/about_classes.rb
+++ Koans/about_classes.rb
@@ -19,7 +19,7 @@ class AboutClasses < Neo::Koan
 
   def test_instance_variables_can_be_set_by_assigning_to_them
     fido = Dog2.new
-    assert_equal :@name [], fido.instance_variables
+    assert_equal :@name ["levi", "louise"], fido.instance_variables
 
     fido.set_name("Fido")
     assert_equal __, fido.instance_variables
